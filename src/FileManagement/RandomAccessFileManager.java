2a5503480c95cf4986f6942d7c8414c64606ff6f
dev-danial <d_alihosseiny@yahoo.com>
2016-05-30 17:40:44 +0430

    initializing continues

--- src/FileManagement/RandomAccessFileManager.java
+++ src/FileManagement/RandomAccessFileManager.java
@@ -1,8 +1,51 @@
 package FileManagement;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.RandomAccessFile;
+
 /**
  * Created by danial on 5/28/16.
  */
 public class RandomAccessFileManager
 {
+    private static String path = "index.txt";
+    private static boolean instanceCreated = false;
+    private static RandomAccessFile instance = null;
+
+    protected RandomAccessFileManager()
+    {
+
+    }
+
+    public static RandomAccessFile getMyInstance()
+    {
+        if(!instanceCreated)
+        {
+            System.out.println("instance creating");
+            instanceCreated = true;
+            try
+            {
+                File file = new File(path);
+                if (file.exists())
+                {
+                    file.delete();
+                    System.out.println("deleting last index");
+                }
+                instance = new RandomAccessFile(path, "rw");
+            } catch (FileNotFoundException e)
+            {
+                e.printStackTrace();
+            }
+        }
+        return instance;
+    }
+
+    @Override
+    protected void finalize() throws Throwable
+    {
+        if(instance != null)
+            instance.close();
+        super.finalize();
+    }
 }
